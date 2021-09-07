82b02cef6224a5bd8e6e4603799402f5a4a881df
= <=>
2021-09-07 18:34:36 +0400

    switched form sqlite database to mysql

--- savele_market/settings.py
+++ savele_market/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'savele_market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'savele_market',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'mysql_user',
+        'PASSWORD': 'password',
     }
 }
 
